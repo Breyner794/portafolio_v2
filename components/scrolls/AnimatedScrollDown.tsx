35f821309a9f043fa3af1748f3e54cb040088d25
Breyner794 <jhoanquilindo@gmail.com>
2025-10-25 18:17:11 -0500

    feat: Añadir propiedad targetSection al componente AnimatedScrollDown para mejorar la navegación

--- components/scrolls/AnimatedScrollDown.tsx
+++ components/scrolls/AnimatedScrollDown.tsx
@@ -4,20 +4,22 @@ import Lottie from 'lottie-react';
 import scrollAnimationData from '@/assets/lottie/scrollDown.json'; 
 
 interface AnimatedScrollDownProps {
-  size?: number; 
+  size?: number;
+  targetSection?: string;
 }
 
-const AnimatedScrollDown: React.FC<AnimatedScrollDownProps> = ({ size = 50 }) => {
+const AnimatedScrollDown: React.FC<AnimatedScrollDownProps> = ({ size = 50, targetSection = '#acerca-de-mi' }) => {
   
   return (
 
-    
-    <Lottie
-      animationData={scrollAnimationData} 
-      loop={true}
-      autoplay={true}
-      style={{ width: size, height: size, cursor: 'pointer' }}
-    />
+    <a href={targetSection} className="cursor-pointer">
+      <Lottie
+        animationData={scrollAnimationData} 
+        loop={true}
+        autoplay={true}
+        style={{ width: size, height: size }}
+      />
+    </a>
     
   );
 };
